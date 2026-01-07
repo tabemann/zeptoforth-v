c659453d626470094623af8c8977329dc75b474d
Travis Bemann <tabemann@gmail.com>
2026-01-06 22:25:09 -0600

    Converted syntax words

--- src/common/syntax.s
+++ src/common/syntax.s
@@ -1,165 +1,165 @@
-@ Copyright (c) 2023-2024 Travis Bemann
-@
-@ Permission is hereby granted, free of charge, to any person obtaining a copy
-@ of this software and associated documentation files (the "Software"), to deal
-@ in the Software without restriction, including without limitation the rights
-@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-@ copies of the Software, and to permit persons to whom the Software is
-@ furnished to do so, subject to the following conditions:
-@ 
-@ The above copyright notice and this permission notice shall be included in
-@ all copies or substantial portions of the Software.
-@ 
-@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-@ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-@ SOFTWARE.
+# Copyright (c) 2023-2026 Travis Bemann
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-        @ Dump the syntax stack
+        # Dump the syntax stack
         define_internal_word "dump-syntax", visible_flag
 _dump_syntax:
-        ldr r0, =syntax_stack + syntax_stack_size - 1
-        ldr r1, =syntax_stack_ptr
-        ldr r1, [r1]
-1:      cmp r0, r1
-        blo 2f
+        li x15, syntax_stack + syntax_stack_size - 1
+        li x14, syntax_stack_ptr
+        lc x14, 0(x14)
+1:      bltu x15, x14, 2f
         push_tos
-        ldrb tos, [r0]
-        subs r0, #1
-        b 1b
+        lbu tos, 0(x15)
+        addi x15, x15 -1
+        j 1b
 2:      push_tos
-        ldr tos, =syntax_stack + syntax_stack_size
-        subs tos, r1
-        bx lr
+        li tos, syntax_stack + syntax_stack_size
+        sub tos, tos, x14
+        ret
         end_inlined
         
-        @ Push a syntax onto the syntax stack
-        @ ( syntax -- )
+        # Push a syntax onto the syntax stack
+        # ( syntax -- )
         define_internal_word "push-syntax", visible_flag
 _push_syntax:
-        push {lr}
-        ldr r0, =syntax_stack_ptr
-        ldr r1, [r0]
-        ldr r2, =syntax_stack
-        cmp r1, r2
-        bne 1f
-        ldr tos, =_syntax_overflow
-        bl _raise
-1:      subs r1, #1
-        strb tos, [r1]
-        str r1, [r0]
+        push ra
+        li x15, syntax_stack_ptr
+        lc x14, 0(x15)
+        li x13, syntax_stack
+        bne x14, x13, 1f
+        li tos, _syntax_overflow
+        call _raise
+1:      addi x14, x14, -1
+        sb tos, 0(x14)
+        sc x14, 0(x15)
         pull_tos
-        pop {pc}
+        pop ra
+        ret
         end_inlined
 
-        @ Verify a syntax on the syntax stack against one syntax
-        @ ( syntax -- )
+        # Verify a syntax on the syntax stack against one syntax
+        # ( syntax -- )
         define_internal_word "verify-syntax", visible_flag
 _verify_syntax:
-        push {lr}
-        ldr r0, =syntax_stack_ptr
-        ldr r1, [r0]
-        ldr r2, =syntax_stack + syntax_stack_size
-        cmp r1, r2
-        bne 1f
-        ldr tos, =_syntax_underflow
-        bl _raise
-1:      ldrb r3, [r1]
-        cmp tos, r3
-        beq 2f
-        ldr tos, =_unexpected_syntax
-        bl _raise
+        push ra
+        li x15, syntax_stack_ptr
+        lc x14, 0(x15)
+        li x13, syntax_stack + syntax_stack_size
+        bne x14, x13, 1f
+        li tos, _syntax_underflow
+        call _raise
+1:      lbu x12, 0(x14)
+        beq tos, x12, 2f
+        li tos, _unexpected_syntax
+        call _raise
 2:      pull_tos
-        pop {pc}
+        pop ra
+        ret
         end_inlined
 
-        @ Get the topmost syntax
+        # Get the topmost syntax
         define_internal_word "get-syntax", visible_flag
-_get_syntax: 
-        ldr r0, =syntax_stack_ptr
-        ldr r1, [r0]
-        ldr r2, =syntax_stack + syntax_stack_size
-        cmp r1, r2
-        bne 1f
-        ldr tos, =_syntax_underflow
-        bl _raise
-1:      push_tos
-        ldrb tos, [r1]
-        bx lr
+_get_syntax:
+        li x15, syntax_stack_ptr
+        lc x14, 0(x15)
+        li x13, syntax_stack + syntax_stack_size
+        push_tos
+        bne x14, x13, 1f
+        li tos, _syntax_underflow
+        call _raise
+1:      lbu tos, 0x(14)
+        ret
         end_inlined
         
-        @ Verify a syntax on the syntax stack against two syntaxes
-        @ ( syntax1 syntax0 -- )
+        # Verify a syntax on the syntax stack against two syntaxes
+        # ( syntax1 syntax0 -- )
         define_internal_word "verify-syntax-2", visible_flag
 _verify_syntax_2:
-        push {lr}
-        ldr r0, =syntax_stack_ptr
-        ldr r1, [r0]
-        ldr r2, =syntax_stack + syntax_stack_size
-        cmp r1, r2
-        bne 1f
-        ldr tos, =_syntax_underflow
-        bl _raise
-1:      ldrb r3, [r1]
-        cmp tos, r3
-        bne 2f
-        pull_tos
-        pull_tos
-        pop {pc}
+        push ra
+        li x15, syntax_stack_ptr
+        lc x14, 0(x15)
+        li x13, syntax_stack + syntax_stack_size
+        bne x14, x13, 1f
+        li tos, _syntax_underflow
+        call _raise
+1:      lbu x12, 0(x14)
+        bne tos, x12, 2f
+        lc tos, cell(dp)
+        addi dp, dp, 2*cell
+        pop ra
+        ret
 2:      pull_tos
-        cmp tos, r3
-        beq 3f
-        ldr tos, =_unexpected_syntax
-        bl _raise
+        beq tos, x12, 3f
+        li tos, _unexpected_syntax
+        call _raise
 3:      pull_tos
-        pop {pc}
+        pop ra
+        ret
         end_inlined
-
-        @ Drop a syntax on the syntax stack
-        @ ( -- )
+        
+        # Drop a syntax on the syntax stack
+        # ( -- )
         define_internal_word "drop-syntax", visible_flag
 _drop_syntax:
-        push {lr}
-        ldr r0, =syntax_stack_ptr
-        ldr r1, [r0]
-        ldr r2, =syntax_stack + syntax_stack_size
-        cmp r1, r2
-        bne 1f
-        ldr tos, =_syntax_underflow
-        bl _raise
-1:      adds r1, #1
-        str r1, [r0]
-        pop {pc}
+        push ra
+        li x15, syntax_stack_ptr
+        lc x14, 0(x15)
+        li x13, syntax_stack + syntax_stack_size
+        bne x14, x13, 1f
+        push_tos
+        li tos, _syntax_underflow
+        call _raise
+1:      addi x14, x14, 1
+        sc x14, 0(x15)
+        pop ra
+        ret
         end_inlined
 
-        @ Unexpected syntax exception
+        # Unexpected syntax exception
         define_internal_word "x-unexpected-syntax", visible_flag
 _unexpected_syntax:
-        push {lr}
+        push ra
         string_ln "unexpected syntax"
-        bl _type
-        pop {pc}
+        call _type
+        pop ra
+        ret
         end_inlined
 
-        @ Syntax underflow
+        # Syntax underflow
         define_internal_word "x-syntax-underflow", visible_flag
 _syntax_underflow:
-        push {lr}
+        push ra
         string_ln "syntax underflow"
-        bl _type
-        pop {pc}
+        call _type
+        pop ra
+        ret
         end_inlined
 
-        @ Syntax overflow
+        # Syntax overflow
         define_internal_word "x-syntax-overflow", visible_flag
 _syntax_overflow:
-        push {lr}
+        push ra
         string_ln "syntax overflow"
-        bl _type
-        pop {pc}
+        call _type
+        pop ra
+        ret
         end_inlined
 
         .ltorg
